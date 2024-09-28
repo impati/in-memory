2a2075d22c7417c3dadab741964bfb947e729ec4
impati <yongs170@naver.com>
2024-09-28 20:09:48 +0900

    feature: 리액티브 프로그래밍으로 전환

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+
 }
 
 group = "org.example"
@@ -9,8 +11,16 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("io.projectreactor:reactor-bom:2023.0.8")
+    }
+}
+
 dependencies {
+    implementation("io.projectreactor:reactor-core")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.test {
